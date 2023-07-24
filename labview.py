f54b6306011cedfb7fd52d2cd89ce61918ad8c50
David Maluenda Niubó <dmaluenda@ub.edu>
2023-07-24 18:06:39 +0200

    working befor big refactoring on labHolo

--- labview.py
+++ labview.py
@@ -1,24 +1,32 @@
-from numba import jit
+# from numba import jit
 import numpy as np
+import os
+import imageio
 
-from interface import IDSCamera
+import interface
 import matplotlib.pyplot as plt
+
+import json
 
 IDS_interface_Obj = None
 roi = None
+ref = None
 
 def init(is16bits=False):
     mode = "Mono12" if is16bits else "Mono8"
     # Obrim la llibreria
     global IDS_interface_Obj
-    IDS_interface_Obj = IDSCamera()
+    try:
+        IDS_interface_Obj = interface.IDSCamera()
+    except interface.ids_peak.NotFoundException:
+        return ["NotFound"], ["-1"]
+
     # Busquem dispositius disponibles
     devicesSerial = []
     devicesName = []
     for camID, cam in enumerate(IDS_interface_Obj.get_devices()):
         devicesSerial.append(cam.SerialNumber())
         devicesName.append(cam.ModelName())
-
 
     # Seleccionem la imatge de sortida
     IDS_interface_Obj.set_pixel_format(mode)
@@ -27,6 +35,8 @@ def init(is16bits=False):
 
 
 def start(cam_id=0, exposure_ms=1, fps=100):
+    if cam_id == -1:
+        return 1, 1, 1, 1  # Dummy values
     # Comencem l'adquisició, que bloqueja canvis "crítics" en la càmera
     global IDS_interface_Obj
 
@@ -47,13 +57,15 @@ def start(cam_id=0, exposure_ms=1, fps=100):
 
     return width, height, true_fps, true_exposure
 
-def capture(cam_id=0, binning=1):
+def capture(cam_id=0, binning=1, use_roi=False):
+    if cam_id == -1:
+        return np.zeros((100, 100), dtype=np.uint16)  # Dummy values
     # Capturem imatges
     global IDS_interface_Obj
     global roi
     image = IDS_interface_Obj.capture(cam_id)[::binning, ::binning]
 
-    if roi is not None:
+    if roi is not None and use_roi:
         roix, roiy, roiw, roih = roi
         image = image[roiy:roiy+roih, roix:roix+roiw]
 
@@ -66,12 +78,16 @@ def stop():
     IDS_interface_Obj.stop_acquisition()
 
 def set_exposure(exposure_ms, cam_id=0):
+    if cam_id == -1:
+        return 1, 1  # Dummy values
     global IDS_interface_Obj
     IDS_interface_Obj.set_exposure_time(exposure_ms*1000, cam_id)  # gets in um
     return [IDS_interface_Obj.get_exposure_time(cam_id)/1000,  # in ms
             IDS_interface_Obj.get_fps(cam_id)]
 
 def set_fps(fps, cam_id=0):
+    if cam_id == -1:
+        return 1, 1  # Dummy values
     global IDS_interface_Obj
     IDS_interface_Obj.set_fps(fps, cam_id)
     return [IDS_interface_Obj.get_fps(cam_id),
@@ -80,6 +96,67 @@ def set_fps(fps, cam_id=0):
 def set_roi(roix, roiy, roiw, roih):
     global roi
     roi = (roix, roiy, roiw, roih)
+
+def set_ref(refx, refy, refw, refh):
+    global ref
+    ref = (refx, refy, refw, refh)
+
+
+def save(image, filename, main_roi=None, ref_roi=None, bit_depth=16):
+    np_fn = filename.replace(".png", ".npy")
+
+    # main_roi implementation
+    roi_xi, roi_yi, roi_xf, roi_yf = main_roi or (None, None, None, None)
+    image2save = image[roi_yi:roi_yf, roi_xi:roi_xf].astype(np.float32)
+
+    ref_xi, ref_yi, ref_xf, ref_yf = ref_roi or (None, None, None, None)
+    ref_mean = image[ref_yi:ref_yf, ref_xi:ref_xf].mean() if ref_roi else 1
+    ref_max = 2**14 if ref_roi else 1
+
+    image2save /= ref_mean
+    image2save16 = (image2save * ref_max).astype(np.uint16)
+
+    # Save as PNG with imageio
+    crop_fn = filename.replace(".png", "_crop.png")
+    if os.path.isfile(crop_fn):
+        os.remove(crop_fn)
+    imageio.imwrite(crop_fn, image2save16)
+
+    # Guardem la imatge com a numpy array
+    if os.path.isfile(np_fn):
+        os.remove(np_fn)
+    np.save(np_fn, image2save)
+
+    try:
+        # Guardem metadades
+        dir, fn = os.path.split(filename)
+        meta_fn = os.path.join(dir, fn.split('_')[0] + ".json")
+        if os.path.isfile(meta_fn):
+            # Load the dictionary from the file
+            with open(meta_fn, 'r') as meta_file:
+                meta_dict = json.load(meta_file)
+        else:
+            meta_dict = {}
+
+        current_dict = {"mean": float(image2save.mean()),
+                        "max": float(image2save.max()),
+                        "min": float(image2save.min())}
+        if main_roi is not None:
+            current_dict.update({'center': ((roi_xi+roi_xf)/2, (roi_yi+roi_yf)/2),
+                                 'size': (roi_xf-roi_xi, roi_yf-roi_yi)})
+
+        if ref_roi is not None:
+            current_dict.update(ref_roi={'center': ((ref_xi+ref_xf)/2, (ref_yi+ref_yf)/2),
+                                         'size': (ref_xf-ref_xi, ref_yf-ref_yi)})
+
+        meta_dict.update({fn: current_dict})
+        # Save the dictionary to the file
+        with open(meta_fn, 'w') as meta_file:
+            json.dump(meta_dict, meta_file, indent=4)
+    except:
+        pass
+
+    return np_fn
 
 
 if __name__ == "__main__":
